e2cb5b4f774fc9a6964e2d04bc9fe4e81ee3888e
Sandra Kåhre <sandra.kahre@gmail.com>
2026-05-13 20:23:33 +0200

    Add comments to blog page

--- blog.php
+++ blog.php
@@ -11,14 +11,19 @@
     <main class="flex flex-row gap-4 p-4 flex-1 bg-offwhite">
         <aside class="flex flex-col gap-4 w-72 shrink-0">
             <?php
+            // PLACEHOLDER: Fetch blogger information from database
             $info_user = [
                 "name" => $blogger["name"] ?? "Unknown Blogger",
                 "username" => $blogger["username"] ?? "place_holder",
                 "bio" => $blogger["bio"] ?? null,
             ];
-            require_once "components/info.php";
+
+            // Include same info component as in profile page
+            // Gets necessary data from $info_user array
+            include "components/info.php";
+
+            include "components/menu.php";
             ?>
-            <?php require_once "components/menu.php"; ?>
         </aside>
         <section class="flex-1 min-w-0">
             <?php require_once "components/content.php"; ?>
